c28af6291cad91ce729fb3d01f67fea80b70b1cd
Shweta kale <shwetakale144@gmail.com>
2025-04-07 09:40:30 +0530

    incorrect cloudfront code

--- deploy-randomm-color/index.ts
+++ deploy-randomm-color/index.ts
@@ -4,28 +4,23 @@ import * as fs from "fs";
 import * as path from "path";
 import { lookup } from "mime-types";
 
-// 1. Create S3 bucket (no ACL!)
-
+// 1. Create S3 bucket (no public access)
 const bucket = new aws.s3.Bucket("idiot-bucket", {
   website: {
       indexDocument: "index.html",
   },
 });
 
-const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
-  bucket: bucket.id,
-  rule: {
-      objectOwnership: "ObjectWriter"
-  }
-});
-
+// 2. Block public access to S3 bucket
 const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
   bucket: bucket.id,
-  blockPublicAcls: false,
+  blockPublicAcls: true,  // Block all public ACLs
+  blockPublicPolicy: true, // Block all public policies
+  restrictPublicBuckets: true,
+  ignorePublicAcls: true,
 });
 
-
-// 4. Upload static files from local directory
+// 3. Upload static files from local directory (without public-read ACL)
 const webDir = "../out";
 
 function crawlDir(dir: string, callback: (filePath: string) => void) {
@@ -47,10 +42,44 @@ crawlDir(webDir, (filePath: string) => {
     bucket: bucket.id,
     source: new pulumi.asset.FileAsset(filePath),
     contentType: lookup(filePath) || undefined,
-    acl: "public-read",
   }, { dependsOn: publicAccessBlock });
 });
 
-// 5. Export bucket name and website endpoint
+// 4. Create CloudFront Distribution (with OAI for secure access)
+const oai = new aws.cloudfront.OriginAccessIdentity("oai", {
+  comment: "OAI for secured S3 access",
+});
+
+const cloudfrontDistribution = new aws.cloudfront.Distribution("web-distribution", {
+  enabled: true,
+  origins: [{
+    originId: bucket.id,
+    domainName: bucket.bucketRegionalDomainName,
+    s3OriginConfig: {
+      originAccessIdentity: oai.cloudfrontAccessIdentityPath,
+    },
+  }],
+  defaultCacheBehavior: {
+    targetOriginId: bucket.id,
+    viewerProtocolPolicy: "redirect-to-https",
+    allowedMethods: ["GET", "HEAD"],
+    cachedMethods: ["GET", "HEAD"],
+    forwardedValues: {
+      queryString: false,
+      cookies: { forward: "none" },
+    },
+  },
+  priceClass: "PriceClass_100", // Use PriceClass_100 for lower-cost edge locations
+  viewerCertificate: {
+    cloudfrontDefaultCertificate: true,
+  },
+  restrictions: {
+    geoRestriction: {
+      restrictionType: "none", // No restrictions on country access
+    },
+  },
+});
+
+// 5. Export bucket name and CloudFront URL
 export const bucketName = bucket.bucket;
-export const bucketWebsiteUrl = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+export const cloudfrontUrl = pulumi.interpolate`https://${cloudfrontDistribution.domainName}`;
